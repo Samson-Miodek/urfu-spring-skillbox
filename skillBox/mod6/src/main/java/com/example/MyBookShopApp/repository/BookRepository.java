a5380622b93ee116935a2972517c698f4b8fd23c
Samson-Miodek <video.kr4.123@gmail.com>
2023-10-16 18:49:40 +0500

    mod6/lesson 6.5

--- skillBox/mod6/src/main/java/com/example/MyBookShopApp/repository/BookRepository.java
+++ skillBox/mod6/src/main/java/com/example/MyBookShopApp/repository/BookRepository.java
@@ -3,6 +3,8 @@ package com.example.MyBookShopApp.repository;
 import com.example.MyBookShopApp.data.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book,Integer> {
 
 }
--- skillBox/mod6/src/main/java/com/example/MyBookShopApp/service/BookService.java
+++ skillBox/mod6/src/main/java/com/example/MyBookShopApp/service/BookService.java
@@ -3,6 +3,8 @@ package com.example.MyBookShopApp.service;
 import com.example.MyBookShopApp.data.book.Book;
 import com.example.MyBookShopApp.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +24,8 @@ public class BookService {
         return bookRepository.findAll();
     }
 
-
+    public Page<Book> getPageOfBooksData(Integer offset, Integer limit){
+        var nextPage = PageRequest.of(offset,limit);
+        return bookRepository.findAll(nextPage);
+    }
 }
